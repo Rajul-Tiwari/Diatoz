2ab7fda87624340bb9f6d21cab4e6dc2e5222b05
Rajul Tiwari <105916922+Rajul-Tiwari@users.noreply.github.com>
2023-09-12 12:10:51 +0530

    Add files via upload

---
+++ Diatoz/Diatoz/src/test/java/com/diatoz/DiatozApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diatoz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiatozApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
